94ded96ac6421dd6b9eac1103ee004719a88edc0
Moulik <moulikbhardwaj@gmail.com>
2020-08-14 00:38:56 +0530

    Fixed framerate issues

--- myGraphics.js
+++ myGraphics.js
@@ -1,179 +1,220 @@
+(function () {
+    {
+        if (document.getElementById("canvas") === null) {
+            const canvas = document.createElement("canvas");
+            canvas.width = 480;
+            canvas.height = 320;
+            canvas.id = "canvas";
+            document.getElementsByTagName("body")[0].appendChild(canvas);
+        }
+        const canvas = document.getElementById("canvas");
+        const ctx = canvas.getContext("2d");
+        this.width = canvas.width;
+        this.height = canvas.height;
+        var Framerate = 60;
+        const globalStyle = {};
+        var color = "rgba(0,0,0,1)";
+        var lineWidth = 1;
 
-if (document.getElementById("canvas") === null) {
-    const canvas = document.createElement("canvas");
-    canvas.width = 480;
-    canvas.height = 320;
-    canvas.id = "canvas";
-    document.getElementsByTagName("body")[0].appendChild(canvas);
-}
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const width = canvas.width;
-const height = canvas.height;
-var Framerate = 60;
-const globalStyle = {};
-var color = "rgba(0,0,0,1)";
-var lineWidth = 1;
+        var cachedData = {};
+        var animationFrame = 0;
+        var lastFrame, startTime;
 
-var doLoop = true;
-var now, before = Date.now();
+        this.setup = () => {
+            ctx.clearRect(0, 0, width, height);
+            strokeRect(0, 0, width - 1, height - 1);
+        }
 
-function setup() {
-    ctx.clearRect(0, 0, width, height);
-    strokeRect(0, 0, width - 1, height - 1);
-}
+        this.setStyle = (style) => {
+            const stylekeys = Object.keys(style);
+            const ctxKeys = Object.keys(Object.getPrototypeOf(ctx));
+            const keys = stylekeys.filter(e => { return ctxKeys.indexOf(e) > -1; });
+            for (key of keys) {
+                ctx[key] = style[key];
+            }
+        }
 
-function setStyle(style){
-    const stylekeys = Object.keys(style);
-    const ctxKeys = Object.keys(Object.getPrototypeOf(ctx));
-    const keys = stylekeys.filter(e=>{return ctxKeys.indexOf(e)>-1;});
-    for(key of keys){
-        ctx[key] = style[key];
+        this.line = (x1, y1, x2, y2, style = {}) => {
+            ctx.save();
+            setStyle(style);
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.stroke();
+            ctx.restore();
+        }
+
+        this.strokeRect = (x1, y1, w, h, style = {}) => {
+            ctx.save();
+            setStyle(style);
+            ctx.beginPath();
+            ctx.rect(x1, y1, w, h);
+            ctx.stroke();
+            ctx.restore();
+        }
+
+        this.fillRect = (x1, y1, w, h, style = {}) => {
+            ctx.save();
+            setStyle(style);
+            ctx.beginPath();
+            ctx.rect(x1, y1, w, h);
+            ctx.fill();
+            ctx.restore();
+        }
+
+        this.point = (x, y, size=1, style = {}) => {
+            ctx.save();
+            setStyle(style);
+            ctx.beginPath()
+            ctx.rect(x, y, size, size);
+            ctx.fill();
+            ctx.restore();
+        }
+
+        this.strokeCircle = (x, y, r, style = {}) => {
+            ctx.save();
+            setStyle(style);
+            ctx.beginPath()
+            ctx.arc(x, y, r, 0, style.angle || Math.PI * 2);
+            ctx.stroke();
+            ctx.restore();
+        }
+
+        this.fillCircle = (x, y, r, style = {}) => {
+            ctx.save();
+            setStyle(style);
+            ctx.beginPath()
+            ctx.arc(x, y, r, 0, style.angle || Math.PI * 2);
+            ctx.fill();
+            ctx.restore();
+        }
+
+        this.strokeEllipse = (x, y, r1, r2, style) => {
+            ctx.save();
+            setStyle(style);
+            ctx.beginPath()
+            ctx.ellipse(x, y, r1, r2, 0, style.angle || Math.PI * 2);
+            ctx.stroke();
+            ctx.restore();
+        }
+
+        this.fillEllipse = (x, y, r1, r2, style = {}) => {
+            ctx.save();
+            setStyle(style);
+            ctx.beginPath()
+            ctx.ellipse(x, y, r1, r2, style.rotation || 0, 0, style.angle || Math.PI * 2);
+            ctx.fill();
+            ctx.restore();
+        }
+
+        this.strokeTriangle = (x1, y1, x2, y2, x3, y3, style = {}) => {
+            ctx.save();
+            setStyle(style);
+            ctx.beginPath()
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.lineTo(x3, y3);
+            ctx.closePath();
+            ctx.stroke();
+            ctx.restore();
+        }
+
+        this.fillTriangle = (x1, y1, x2, y2, x3, y3, style = {}) => {
+            ctx.save();
+            setStyle(style);
+            ctx.beginPath()
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.lineTo(x3, y3);
+            ctx.closePath();
+            ctx.stroke();
+            ctx.fill();
+            ctx.restore();
+        }
+
+        this.text = (text, x, y, style = {}) => {
+            ctx.save();
+            setStyle(style);
+            if (style.maxWidth) {
+                ctx.strokeText(text, x, y, style.maxWidth);
+            }
+            else {
+                ctx.strokeText(text, x, y);
+            }
+            ctx.restore();
+        }
+
+        this.background = (color) => {
+            ctx.save();
+            ctx.beginPath();
+            ctx.fillStyle = color;
+            ctx.rect(0, 0, width, height);
+            ctx.fill();
+            ctx.restore();
+        }
+
+        this.image = (src, dx, dy, style = {}) => {
+            if (typeof src == "string") {
+                if (cachedData[src] == undefined) {
+                    const img = cachedData[src] || document.createElement("img");
+                    img.src = src;
+
+                    img.addEventListener("load", e => {
+                        cachedData[src] = img;
+                        ctx.save();
+                        setStyle(style);
+                        ctx.drawImage(img, dx, dy);
+                        ctx.restore();
+                    });
+                }
+                else {
+                    ctx.save();
+                    setStyle(style);
+                    ctx.drawImage(cachedData[src], dx, dy);
+                    ctx.restore();
+                }
+            }
+            else if (src != null && typeof src === "object") {
+                ctx.save();
+                setStyle(style);
+                ctx.drawImage(img, dx, dy);
+                ctx.restore();
+            }
+        }
+
+
+        this.doLoop = () => {
+            startTime = window.performance.now();
+            lastFrame = window.performance.now();
+            animationFrame = requestAnimationFrame(loop);
+        }
+
+        this.noLoop = () => {
+            cancelAnimationFrame(animationFrame);
+            lastFrame = undefined;
+            startTime = undefined;
+            animationFrame = undefined;
+        }
+        this.draw = () => {
+
+        };
+
+        this.loop = (time) => {
+            if(startTime === undefined){
+                startTime = time;
+                lastFrame = 0;
+            }
+            else{
+                const curFrame = time-startTime;
+                draw(curFrame-lastFrame);
+                lastFrame=curFrame;
+            }
+            animationFrame = requestAnimationFrame(loop);
+        }
+        
+        this.framerate = (fps) => {
+            Framerate = fps;
+        }
+        this.doLoop();
     }
-}
-
-function line(x1, y1, x2, y2, style = {}) {
-    ctx.save();
-    setStyle(style);
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-    ctx.restore();
-}
-
-function strokeRect(x1, y1, w, h, style = {}) {
-    ctx.save();
-    setStyle(style);
-    ctx.beginPath();
-    ctx.rect(x1, y1, w, h);
-    ctx.stroke();
-    ctx.restore();
-}
-
-function fillRect(x1, y1, w, h, style = {}) {
-    ctx.save();
-    setStyle(style);
-    ctx.beginPath();
-    ctx.rect(x1, y1, w, h);
-    ctx.fill();
-    ctx.restore();
-}
-
-function point(x, y, style = {}) {
-    ctx.save();
-    setStyle(style);
-    ctx.beginPath()
-    ctx.rect(x, y, 1, 1);
-    ctx.fill();
-    ctx.restore();
-}
-
-function strokeCircle(x, y, r, style = {}) {
-    ctx.save();
-    setStyle(style);
-    ctx.beginPath()
-    ctx.arc(x, y, r, 0, style.angle||Math.PI * 2);
-    ctx.stroke();
-    ctx.restore();
-}
-
-function fillCircle(x, y, r, style = {}) {
-    ctx.save();
-    setStyle(style);
-    ctx.beginPath()
-    ctx.arc(x, y, r, 0, style.angle||Math.PI * 2);
-    ctx.fill();
-    ctx.restore();
-}
-
-function strokeEllipse(x, y, r1, r2, style){
-    ctx.save();
-    setStyle(style);
-    ctx.beginPath()
-    ctx.ellipse(x, y, r1, r2, 0, style.angle || Math.PI * 2);
-    ctx.stroke();
-    ctx.restore();
-}
-
-function fillEllipse(x, y, r1, r2, style = {}) {
-    ctx.save();
-    setStyle(style);
-    ctx.beginPath()
-    ctx.ellipse(x, y, r1, r2, style.rotation||0, 0, style.angle || Math.PI * 2);
-    ctx.fill();
-    ctx.restore();
-}
-
-function strokeTriangle(x1, y1, x2, y2, x3, y3, style = {}) {
-    ctx.save();
-    setStyle(style);
-    ctx.beginPath()
-    ctx.moveTo(x1,y1);
-    ctx.lineTo(x2,y2);
-    ctx.lineTo(x3,y3);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.restore();
-}
-
-function fillTriangle(x1, y1, x2, y2, x3, y3, style = {}) {
-    ctx.save();
-    setStyle(style);
-    ctx.beginPath()
-    ctx.moveTo(x1,y1);
-    ctx.lineTo(x2,y2);
-    ctx.lineTo(x3,y3);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.fill();
-    ctx.restore();
-}
-
-function text(text, x, y, style = {}){
-    ctx.save();
-    setStyle(style);
-    if(style.maxWidth){
-        ctx.strokeText(text,x,y,style.maxWidth);
-    }
-    else{
-        ctx.strokeText(text,x,y);
-    }
-    ctx.restore();
-}
-
-function background(color) {
-    ctx.save();
-    ctx.beginPath();
-    ctx.fillStyle = color;
-    ctx.rect(0,0,width,height);
-    ctx.fill();
-    ctx.restore();
-}
-
-
-function doLoop(){
-    doLoop = true;
-}
-
-function noLoop(){
-    doLoop = false;
-}
-
-var draw = function(delta){
-
-};
-
-requestAnimationFrame(function loop(){
-    requestAnimationFrame(loop);
-    now = Date.now();
-    const fps = 1000/Math.round(now-before);
-    if(doLoop && fps<=Framerate){
-        draw(Math.round(now-before));
-        before = now;
-    }
-})
-
-function framerate(fps){
-    Framerate = fps;
-}
+}());
--- test.js
+++ test.js
@@ -1,8 +1,37 @@
 setup();
-
+var pr = 0;
+var sign = 1;
+var speed = {x:(Math.random()-0.3), y:(Math.random()-0.3)};
+var pos = {x:200,y:0};
+var tmp = {};
+const fps = document.createElement("div");
+var m_delta = 16;
+setInterval(()=>{fps.textContent=Math.floor(1000/m_delta);},1000);
+document.getElementsByTagName("body")[0].appendChild(fps);
+var edgeLength = 10;
 draw = (delta)=>{
+    m_delta = delta;
     background("#333333FF");
-    setStyle({strokeStyle:'white', lineCap:"round", lineWidth:1, strokeStyle:"white"})
-    text("Hello, World! I am Moulik Bhardwaj", 300,200);
-    line(100,100,300,300);
+    setStyle({strokeStyle:'white', lineCap:"round", lineWidth:10, fillStyle:"white"})
+    tmp.y = pos.y + speed.y*delta;
+    tmp.x = pos.x + speed.x*delta;
+    if(tmp.y+edgeLength>height){
+        tmp.y = height-edgeLength;
+        speed.y = -Math.abs(speed.y);
+    }
+    else if(tmp.y<0){
+        tmp.y = 0;
+        speed.y = Math.abs(speed.y);
+    }
+    if(tmp.x+edgeLength>width){
+        tmp.x = width-edgeLength;
+        speed.x = -Math.abs(speed.x);
+    } 
+    else if(tmp.x < 0){
+        tmp.x = 0;
+        speed.x = Math.abs(speed.x);
+    }
+    pos.x = tmp.x;
+    pos.y = tmp.y;
+    point(pos.x, pos.y,edgeLength);
 }
